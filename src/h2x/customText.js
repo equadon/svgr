96ae042a9d993d399bb677647acf6c7a65be4241
Niklas Persson <equadon@gmail.com>
2018-04-23 19:06:18 +0200

    Convert <svg> to groups with className svg-item

--- src/h2x/customText.js
+++ src/h2x/customText.js
@@ -4,14 +4,21 @@ const customText = (text, tags) => {
   const split = text.split('=')
   const attr = split[0]
   const value = split[1]
-  const tagList = tags.split(',')
+  const tagList = (tags === undefined) ? [] : tags.split(',')
+  let props = null
 
-  return (text, tags) => ({
+  return (text) => ({
     visitor: {
       JSXElement: {
         enter(path) {
-          if (tagList.includes(path.node.name) && !path.node.attributes.some(a => a && a.name === attr)) {
-            const props = new JSXAttribute()
+          if (path.node.name === 'svg') {
+            props = new JSXAttribute()
+            props.name = 'className'
+            props.value = 'svg-item'
+            path.node.name = 'g'
+            path.node.attributes = [props]
+          } else if (tagList.includes(path.node.name) && !path.node.attributes.some(a => a && a.name === attr)) {
+            props = new JSXAttribute()
             props.name = attr
             props.value = value
             props.literal = true
